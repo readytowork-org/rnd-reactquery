5a5a733715fb3abf22739e91bf30555b76ddfbea
Narayanz <benarayanz@gmail.com>
2021-07-11 00:04:38 +0545

    added people components

--- src/components/People.js
+++ src/components/People.js
@@ -1,6 +1,35 @@
 import React from "react";
+import { useQuery, QueryClient, QueryClientProvider } from "react-query";
+import Person from "./Person";
+const queryClient = new QueryClient();
+
+const fetchPeople = async () => {
+  const res = await fetch("http://swapi.dev/api/people/");
+  return res.json();
+};
 
 const People = () => {
-  return <nav>People</nav>;
+  const { data, status } = useQuery("people", fetchPeople);
+
+  return (
+    <div>
+      <h2>People</h2>
+      {status === "loading" && <div>Loading data</div>}
+      {status === "error" && <div>Error fetching</div>}
+      {status === "success" && (
+        <div>
+          {data.results.map((person, key) => (
+            <Person key={key} person={person} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
-export default People;
+export default function Wraped() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <People />
+    </QueryClientProvider>
+  );
+}
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,6 +1,35 @@
 import React from "react";
+import { useQuery, QueryClient, QueryClientProvider } from "react-query";
+import Planet from "./Planet";
+const queryClient = new QueryClient();
+
+const fetchPlanets = async () => {
+  const res = await fetch("http://swapi.dev/api/planets/");
+  return res.json();
+};
 
 const Planets = () => {
-  return <nav>Planets</nav>;
+  const { data, status } = useQuery("planets", fetchPlanets);
+
+  return (
+    <div>
+      <h2>Planets</h2>
+      {status === "loading" && <div>Loading data</div>}
+      {status === "error" && <div>Error fetching</div>}
+      {status === "success" && (
+        <div>
+          {data.results.map((planet, key) => (
+            <Planet key={key} planet={planet} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
-export default Planets;
+export default function Wraped() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Planets />
+    </QueryClientProvider>
+  );
+}
