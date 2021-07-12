f145ab909341e360c27aa1948c4bf304371a73bf
Narayanz <benarayanz@gmail.com>
2021-07-12 10:59:56 +0545

    added search on reactquery

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,25 +1,33 @@
 import React, { useState } from "react";
 import { useQuery, QueryClient, QueryClientProvider } from "react-query";
-import { Pagination } from "antd";
+import { Pagination, Input } from "antd";
 import User from "./User";
 const queryClient = new QueryClient();
 
 const fetchUsers = async ({ queryKey }) => {
-  const [key, page] = queryKey;
-  const res = await fetch(`http://localhost:8000/user/?page=${page}`);
+  const [key, page, keyword] = queryKey;
+  const res = await fetch(
+    `http://localhost:8000/user/?page=${page}&keyword=${keyword}`
+  );
   return res.json();
 };
 
 const Users = () => {
   const [page, setPage] = useState(1);
-  const { data, status } = useQuery(["users", page], fetchUsers, {
+  const [keyword, setKeyword] = useState("");
+  const { data, status } = useQuery(["users", page, keyword], fetchUsers, {
     keepPreviousData: true,
     refetchOnWindowFocus: false,
   });
   console.log(data?.data?.rows, "here is data");
+  const { Search } = Input;
 
   const onChange = (pageNumber) => {
     setPage(pageNumber);
+  };
+
+  const onSearch = async (value) => {
+    setKeyword(value);
   };
 
   return (
@@ -29,6 +37,13 @@ const Users = () => {
       {status === "error" && <div>Error fetching</div>}
       {status === "success" && (
         <>
+          <Search
+            placeholder="search..."
+            onSearch={onSearch}
+            style={{ width: 200 }}
+          />
+          <br />
+          <br />
           <Pagination
             defaultCurrent={page}
             total={data?.data?.total_rows}
